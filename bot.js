e49aa09d7e3d047c6b1e8027bb46ce0b6b278e7c
hieudn1995 <53845464+hieudn1995@users.noreply.github.com>
2019-08-21 03:37:05 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
- 
+const args = message.content.trim().split(/ +/g);
 
 client.on('ready', () => { // know if our bot is online
 
@@ -21,7 +21,37 @@ client.on('message', message => { // When the message a message is executed
        }
 
 });
+// Defining the question...
+let question = [];
 
+for (let i = 1; i < args.length; i++) {
+  if (args[i].startsWith('"')) break;
+  else question.push(args[i]);
+}
+
+question = question.join(' ');
+
+// Defining the choices...
+const choices = [];
+
+const regex = /(["'])((?:\\\1|\1\1|(?!\1).)*)\1/g;
+let match;
+while (match = regex.exec(args.join(' '))) choices.push(match[2]);
+
+// Creating and sending embed...
+let content = [];
+for (let i = 0; i < choices.length; i++) content.push(`${options[i]} ${choices[i]}`);
+content = content.join('\n');
+
+var embed = new Discord.RichEmbed()
+  .setColor('#8CD7FF')
+  .setTitle(`**${question}**`)
+  .setDescription(content);
+
+message.channel.send(`:bar_chart: ${message.author} started a poll.`, embed)
+  .then(async m => {
+    for (let i = 0; i < choices.length; i++) await m.react(options[i]);
+  });
  
 
 // THIS  MUST  BE  THIS  WAY
